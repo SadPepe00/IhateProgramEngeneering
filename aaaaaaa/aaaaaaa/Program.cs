dbc043fa2f3395dfe66e4e959da42776fbdb061a
SukhanDmitry <suhan.gg01@gmail.com>
2023-11-16 19:48:18 +0300

    code

---
+++ aaaaaaa/aaaaaaa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace aaaaaaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение простой задачи");
+        }
+    }
+}
